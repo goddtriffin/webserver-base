4ec184eb234d79b7d0d6d3d31c7ee273b288eab1
Todd Griffin <toddgriffin@protonmail.com>
2024-02-28 19:58:59 -0800

    Refactor function which resolves the real client IP address to be dynamic from a prioritized list of HTTP headers to check.

--- webserver_base/src/axum_plausible_analytics.rs
+++ webserver_base/src/axum_plausible_analytics.rs
@@ -3,6 +3,7 @@ use axum::http::{HeaderMap, StatusCode};
 use plausible_rs::{EventHeaders, EventPayload, Plausible, PAGEVIEW_EVENT};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tracing::{error, info, instrument, warn};
@@ -79,74 +80,92 @@ impl AxumPlausibleAnalyticsHandler {
 
     /// Determine the client's actual IP address (not the IP address of any Proxies).
     #[instrument(skip_all)]
-    fn resolve_true_client_ip_address(addr: SocketAddr, headers: HeaderMap) -> String {
-        if let Some(forwarded) = headers.get("Forwarded") {
-            match forwarded.to_str() {
-                Ok(forwarded) => {
-                    warn!("TODO - handle the HTTP 'Forwarded' header: {forwarded}");
+    fn resolve_true_client_ip_address(socket_addr: SocketAddr, header_map: HeaderMap) -> String {
+        // prioritized list of HTTP headers that may contain the client's true IP address
+        // (top-most entry is the most trusted)
+        let prioritized_headers: Vec<&str> = vec![
+            // Cloudflare
+            "True-Client-IP",
+            "CF-Connecting-IP",
+            // standard
+            "X-Forwarded-For",
+            "X-Real-IP",
+            "Forwarded",
+            // backup (Axum's 'SocketAddr' IP; definitely a proxy)
+            "socket_addr",
+        ];
+
+        // get the value of each prioritized header (if it exists)
+        let prioritized_headers_values: HashMap<&str, Option<String>> = prioritized_headers.iter().map(|prioritized_header| {
+            if *prioritized_header == "socket_addr" {
+                // this is a backup in the case we fail to get 'true' IP address
+                // (usually a Proxy IP)
+                return (*prioritized_header, Some(socket_addr.ip().to_string()));
+            }
+
+            let header_value: Option<String> = match header_map.get(*prioritized_header) {
+                // prioritized header exists in the HeaderMap
+                Some(header_value) => {
+                    match header_value.to_str() {
+                        // successfully parsed HTTP header value
+                        Ok(header_value) => {
+                            if *prioritized_header == "X-Forwarded-For" {
+                                info!("full HTTP 'X-Forwarded-For' header IP list: {header_value}");
+
+                                // 'X-Forwarded-For' header may contain multiple IP addresses
+                                let parts: Vec<&str> = header_value.split(',').collect();
+
+                                // if there are multiple entries in this list, the left-most entry is the
+                                // client's actual IP address (all other entries are Network Proxies)
+                                let x_forwarded_for_client_ip: Option<&&str> = parts.first();
+                                if let Some(x_forwarded_for_client_ip) = x_forwarded_for_client_ip {
+                                    let x_forwarded_for: String = (*x_forwarded_for_client_ip).to_string();
+                                    Some(x_forwarded_for)
+                                } else {
+                                    // zero IP addresses found in 'X-Forwarded-For' header
+                                    None
+                                }
+                            } else {
+                                // all other headers are single IP addresses
+                                Some(header_value.to_string())
+                            }
+                        }
+                        // failed to parse HTTP header value
+                        Err(to_str_error) => {
+                            error!("failed to parse HTTP '{prioritized_header}' header value: {to_str_error}");
+                            None
+                        }
+                    }
                 }
-                Err(to_str_error) => {
-                    error!("failed to parse HTTP 'Forwarded' header value: {to_str_error}");
-                }
+                // prioritized header does not exist in the HeaderMap
+                None => {
+                    None
+                },
+            };
+            (*prioritized_header, header_value)
+        }).collect();
+        info!(
+            "Client IP headers: {:?}",
+            prioritized_headers_values.clone()
+        );
+
+        // choose the first prioritized header that exists
+        for prioritized_header in prioritized_headers {
+            if let Some(Some(header_value)) = prioritized_headers_values.get(prioritized_header) {
+                info!(
+                    "chose HTTP '{prioritized_header}' header for true client IP: '{header_value}'"
+                );
+                return header_value.to_string();
             }
         }
 
-        if let Some(x_real_ip) = headers.get("X-Real-IP") {
-            match x_real_ip.to_str() {
-                Ok(x_real_ip) => {
-                    warn!("TODO - handle the HTTP 'X-Real-IP' header: {x_real_ip}");
-                }
-                Err(to_str_error) => {
-                    error!("failed to parse HTTP 'X-Real-IP' header value: {to_str_error}");
-                }
-            }
-        }
-
-        if let Some(cf_connecting_ip) = headers.get("CF-Connecting-IP") {
-            match cf_connecting_ip.to_str() {
-                Ok(cf_connecting_ip) => {
-                    warn!("TODO - handle the HTTP 'CF-Connecting-IP' header: {cf_connecting_ip}");
-                }
-                Err(to_str_error) => {
-                    error!("failed to parse HTTP 'CF-Connecting-IP' header value: {to_str_error}");
-                }
-            }
-        }
-
-        if let Some(true_client_ip) = headers.get("True-Client-IP") {
-            match true_client_ip.to_str() {
-                Ok(true_client_ip) => {
-                    warn!("TODO - handle the HTTP 'True-Client-IP' header: {true_client_ip}");
-                }
-                Err(to_str_error) => {
-                    error!("failed to parse HTTP 'True-Client-IP' header value: {to_str_error}");
-                }
-            }
-        }
-
-        if let Some(x_forwarded_for) = headers.get("X-Forwarded-For") {
-            match x_forwarded_for.to_str() {
-                Ok(x_forwarded_for) => {
-                    let parts: Vec<&str> = x_forwarded_for.split(',').collect();
-
-                    // if there are multiple entries in this list, the left-most entry is the
-                    // client's actual IP address (all other entries are Network Proxies)
-                    let x_forwarded_for_client_ip: Option<&&str> = parts.first();
-                    if let Some(x_forwarded_for_client_ip) = x_forwarded_for_client_ip {
-                        let x_forwarded_for: String = (*x_forwarded_for_client_ip).to_string();
-                        info!("found HTTP 'X-Forwarded-For' header: {x_forwarded_for}");
-                        return x_forwarded_for;
-                    };
-                }
-                Err(to_str_error) => {
-                    error!("failed to parse HTTP 'X-Forwarded-For' header value: {to_str_error}");
-                }
-            }
-        }
-
-        // failed to get 'true' IP address - default to the SocketAddr (proxy IP)
-        let axum_socketaddr_ip: String = addr.ip().to_string();
-        warn!("Failed to resolve client's true IP address. Defaulting to Axum's 'SocketAddr' IP (which is usually the IP address of an intermediary Network Proxy): {axum_socketaddr_ip}");
-        axum_socketaddr_ip
+        // THIS SHOULD NEVER BE HIT because 'socket_addr' always exists
+        error!("failed to find any prioritized HTTP headers for true client IP address");
+        prioritized_headers_values
+            .get("socket_addr")
+            .unwrap()
+            .as_ref()
+            .unwrap()
+            .to_string()
     }
 }
